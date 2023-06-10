bda348787ef2dc72dca9c7b6cdfbf4ebd6b83ff4
Murilo Dutra <murilodut@gmail.com>
2023-06-10 20:06:19 -0300

    Check NODE_ENV before connecting to Redis

--- src/redis/redis.module.ts
+++ src/redis/redis.module.ts
@@ -9,10 +9,14 @@ import Redis from 'ioredis';
     {
       provide: 'REDIS_CLIENT',
       useFactory: (configService: ConfigService) => {
-        return new Redis({
-          host: configService.get<string>('REDIS_HOST'),
-          port: configService.get<number>('REDIS_PORT'),
-        });
+        const NODE_ENV = configService.get<string>('NODE_ENV');
+
+        if (NODE_ENV !== 'test') {
+          return new Redis({
+            host: configService.get<string>('REDIS_HOST'),
+            port: configService.get<number>('REDIS_PORT'),
+          });
+        }
       },
       inject: [ConfigService],
     },
