97e57acffde500fdefb7f764a694677484dca06d
Murilo Dutra <murilodut@gmail.com>
2023-06-09 21:10:40 -0300

    Install and import @nestjs/config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CustomersModule } from './customers/customers.module';
 
 @Module({
-  imports: [CustomersModule],
+  imports: [ConfigModule.forRoot(), CustomersModule],
 })
 export class AppModule {}
