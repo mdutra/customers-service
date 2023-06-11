791ab017a7829f320002dcb813fcea03764cc69a
Murilo Dutra <murilodut@gmail.com>
2023-06-10 23:11:15 -0300

    Check Redis status (502 error)

--- src/redis/redis.service.ts
+++ src/redis/redis.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, Inject } from '@nestjs/common';
+import { Injectable, Inject, BadGatewayException } from '@nestjs/common';
 import Redis, { ReplyError } from 'ioredis';
 import { v4 as uuidv4 } from 'uuid';
 
@@ -7,6 +7,8 @@ export class RedisService {
   constructor(@Inject('REDIS_CLIENT') private readonly redisClient: Redis) {}
 
   async getHash(prefix: string, key: string): Promise<any> {
+    this.checkRedisStatus();
+
     try {
       return this.redisClient.hgetall(`${prefix}:${key}`);
     } catch (err) {
@@ -20,11 +22,19 @@ export class RedisService {
   }
 
   async setHash(prefix: string, data: Record<string, string>): Promise<string> {
+    this.checkRedisStatus();
+
     const values = Object.entries(data).flat();
     const id = uuidv4();
 
     await this.redisClient.hmset(`${prefix}:${id}`, ...values);
 
     return id;
+  }
+
+  private checkRedisStatus() {
+    if (this.redisClient.status === 'reconnecting') {
+      throw new BadGatewayException('Cache unavailable');
+    }
   }
 }
