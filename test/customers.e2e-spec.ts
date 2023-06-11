9a15c67c45eafa3249652661681274f0631b2f62
Murilo Dutra <murilodut@gmail.com>
2023-06-11 18:07:16 -0300

    Add end-to-end tests

---
+++ test/customers.e2e-spec.ts
@@ -0,0 +1,154 @@
+import * as request from 'supertest';
+import { Test } from '@nestjs/testing';
+import { CustomersModule } from '../src/customers/customers.module';
+import { INestApplication } from '@nestjs/common';
+
+describe('/customers (e2e)', () => {
+  let app: INestApplication;
+  const redisClient = {
+    status: '',
+    hgetall: jest.fn().mockReturnValue({
+      name: 'abc',
+      document: '123',
+    }),
+    hmset: jest.fn(),
+    multi: jest.fn().mockReturnValue({
+      hmset: jest.fn().mockReturnThis(),
+      del: jest.fn().mockReturnThis(),
+      exec: jest.fn(),
+    }),
+  };
+
+  beforeAll(async () => {
+    const moduleRef = await Test.createTestingModule({
+      imports: [CustomersModule],
+    })
+      .overrideProvider('REDIS_CLIENT')
+      .useValue(redisClient)
+      .compile();
+
+    app = moduleRef.createNestApplication();
+    await app.init();
+  });
+
+  afterAll(async () => {
+    await app.close();
+  });
+
+  describe('GET', () => {
+    it('should find customer by ID', async () => {
+      const id = '456';
+
+      const response = await request(app.getHttpServer()).get(
+        `/customers/${id}`,
+      );
+
+      expect(response.status).toBe(200);
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(redisClient.hgetall).toBeCalled();
+      expect(response.body).toEqual({ id, ...redisClient.hgetall() });
+      redisClient.hgetall.mockClear();
+    });
+
+    it('should not find customer by ID', async () => {
+      const id = '456';
+      redisClient.hgetall.mockReturnValueOnce({});
+
+      const response = await request(app.getHttpServer()).get(
+        `/customers/${id}`,
+      );
+
+      expect(response.status).toBe(404);
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(redisClient.hgetall).toBeCalled();
+      expect(response.body).toEqual({ message: 'Not Found', statusCode: 404 });
+      redisClient.hgetall.mockClear();
+    });
+
+    it('cache should be unavailable', async () => {
+      const id = '456';
+      const status = jest.replaceProperty(
+        redisClient,
+        'status',
+        'reconnecting',
+      );
+
+      const response = await request(app.getHttpServer()).get(
+        `/customers/${id}`,
+      );
+
+      expect(response.status).toBe(502);
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(redisClient.hgetall).not.toBeCalled();
+      expect(response.body).toEqual({
+        error: 'Bad Gateway',
+        statusCode: 502,
+        message: 'Cache unavailable',
+      });
+      status.restore();
+    });
+  });
+
+  describe('POST', () => {
+    it('should save new customer', async () => {
+      const requestBody = { name: 'foo', document: '456' };
+
+      const response = await request(app.getHttpServer())
+        .post('/customers')
+        .send(requestBody);
+
+      expect(response.status).toBe(201);
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(redisClient.hmset).toBeCalled();
+      expect(response.body).toEqual(expect.objectContaining(requestBody));
+      expect(response.body.id).toEqual(expect.any(String));
+      expect(response.body.id.length).toBeGreaterThan(0);
+      redisClient.hmset.mockClear();
+    });
+  });
+
+  describe('PUT', () => {
+    it('should update customer data', async () => {
+      const id = '456';
+      const requestBody = { name: 'bar', document: '789' };
+
+      const response = await request(app.getHttpServer())
+        .put(`/customers/${id}`)
+        .send(requestBody);
+
+      expect(response.status).toBe(200);
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body).toEqual({
+        id,
+        ...requestBody,
+      });
+    });
+
+    it('should not find customer when updating', async () => {
+      const id = '456';
+      const requestBody = { name: 'baz', document: '1000' };
+      redisClient.hgetall.mockReturnValueOnce({});
+
+      const response = await request(app.getHttpServer())
+        .put(`/customers/${id}`)
+        .send(requestBody);
+
+      expect(response.status).toBe(404);
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body).toEqual({ message: 'Not Found', statusCode: 404 });
+    });
+
+    it('should update customer ID and customer data', async () => {
+      const id = '2000';
+      const requestBody = { id: '3000', name: 'foo bar', document: '200' };
+
+      const response = await request(app.getHttpServer())
+        .put(`/customers/${id}`)
+        .send(requestBody);
+
+      expect(response.status).toBe(200);
+      expect(response.headers['content-type']).toMatch(/json/);
+      expect(response.body).toEqual(requestBody);
+    });
+  });
+});
