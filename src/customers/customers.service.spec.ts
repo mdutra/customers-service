d474c397b91b1a6bc877f623ffc5acb4e7fb4685
Murilo Dutra <murilodut@gmail.com>
2023-06-13 02:21:46 -0300

    Unit test CustomersService

--- src/customers/customers.service.spec.ts
+++ src/customers/customers.service.spec.ts
@@ -1,18 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { CustomersModule } from './customers.module';
 import { CustomersService } from './customers.service';
+import { CustomersRepository } from './customers.repository';
+
+const result = { id: '1', name: 'a', document: 'b' };
 
 describe('CustomersService', () => {
   let service: CustomersService;
+  let repository: CustomersRepository;
 
-  beforeEach(async () => {
+  beforeAll(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [CustomersService],
+      imports: [CustomersModule],
     }).compile();
 
     service = module.get<CustomersService>(CustomersService);
+    repository = module.get<CustomersRepository>(CustomersRepository);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should find one', async () => {
+    jest.spyOn(repository, 'findById').mockResolvedValue(result);
+
+    const customer = await service.findOne('1');
+
+    expect(customer).toEqual(result);
+  });
+
+  it('should not find one', async () => {
+    jest.spyOn(repository, 'findById').mockResolvedValue(null);
+
+    const promise = service.findOne('1');
+
+    expect(promise).rejects.toThrow();
+  });
+
+  it('should create', async () => {
+    jest.spyOn(repository, 'create').mockResolvedValue(result);
+
+    const customer = await service.create({ name: 'c', document: 'd' });
+
+    expect(customer).toEqual(result);
+  });
+
+  it('should update', async () => {
+    jest.spyOn(repository, 'findByIdAndUpdate').mockResolvedValue(result);
+
+    const customer = await service.update('1', {
+      id: '2',
+      name: 'a',
+      document: 'b',
+    });
+
+    expect(customer).toEqual(result);
+  });
+
+  it('should not find when trying to update', async () => {
+    jest.spyOn(repository, 'findByIdAndUpdate').mockResolvedValue(null);
+
+    const promise = service.update('1', {
+      id: '2',
+      name: 'a',
+      document: 'b',
+    });
+
+    expect(promise).rejects.toThrow();
   });
 });
