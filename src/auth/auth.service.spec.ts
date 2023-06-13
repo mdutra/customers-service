3f0c61a43d7fd607921a77832603ad7bfb4c8695
Murilo Dutra <murilodut@gmail.com>
2023-06-13 11:02:23 -0300

    Unit test AuthService

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -1,18 +1,60 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import { ConfigService } from '@nestjs/config';
+import { HttpService } from '@nestjs/axios';
+import { AuthModule } from './auth.module';
 import { AuthService } from './auth.service';
+import { axiosMock } from '../../test/mock/axios.mock';
+import { AxiosError } from 'axios';
 
 describe('AuthService', () => {
   let service: AuthService;
+  let httpService: HttpService;
+  let configService: ConfigService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [AuthService],
-    }).compile();
+      imports: [AuthModule],
+    })
+      .overrideProvider(HttpService)
+      .useValue(axiosMock)
+      .compile();
 
     service = module.get<AuthService>(AuthService);
+    httpService = module.get<HttpService>(HttpService);
+    configService = module.get<ConfigService>(ConfigService);
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should not throw errors', () => {
+    jest.spyOn(configService, 'get').mockReturnValue('');
+
+    const promise = service.verify('abc');
+
+    expect(promise).resolves.toBeUndefined;
+  });
+
+  it('should throw unauthorized', () => {
+    jest.spyOn(configService, 'get').mockReturnValue('');
+    axiosMock.axiosRef.mockReturnValueOnce({
+      data: { active: false },
+    });
+
+    const promise = service.verify('abc');
+
+    expect(promise).rejects.toThrow('Unauthorized');
+  });
+
+  it('should throw SSO unavailable', () => {
+    jest.spyOn(configService, 'get').mockReturnValue('');
+    axiosMock.axiosRef.mockImplementationOnce(() => {
+      throw new AxiosError();
+    });
+
+    const promise = service.verify('abc');
+
+    expect(promise).rejects.toThrow('SSO unavailable');
   });
 });
