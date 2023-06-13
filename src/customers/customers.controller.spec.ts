8e6d5564e2a86785a5b07b8c8d0514096809a7b0
Murilo Dutra <murilodut@gmail.com>
2023-06-13 01:48:04 -0300

    Unit test CustomersController

--- src/customers/customers.controller.spec.ts
+++ src/customers/customers.controller.spec.ts
@@ -1,18 +1,53 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { CustomersController } from './customers.controller';
+import { CustomersService } from './customers.service';
+import { CustomersModule } from './customers.module';
 
 describe('CustomersController', () => {
   let controller: CustomersController;
+  let service: CustomersService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      controllers: [CustomersController],
+      imports: [CustomersModule],
     }).compile();
 
     controller = module.get<CustomersController>(CustomersController);
+    service = module.get<CustomersService>(CustomersService);
   });
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findOne', async () => {
+    const result = { id: '1', name: 'a', document: 'b' };
+    jest.spyOn(service, 'findOne').mockResolvedValue(result);
+
+    const customer = await controller.findOne('');
+
+    expect(customer).toBe(result);
+  });
+
+  it('create', async () => {
+    const result = { id: '1', name: 'a', document: 'b' };
+    jest.spyOn(service, 'create').mockResolvedValue(result);
+
+    const customer = await controller.create({ name: 'c', document: 'd' });
+
+    expect(customer).toBe(result);
+  });
+
+  it('update', async () => {
+    const result = { id: '1', name: 'a', document: 'b' };
+    jest.spyOn(service, 'update').mockResolvedValue(result);
+
+    const customer = await controller.update('1', {
+      id: '2',
+      name: 'c',
+      document: 'd',
+    });
+
+    expect(customer).toBe(result);
   });
 });
